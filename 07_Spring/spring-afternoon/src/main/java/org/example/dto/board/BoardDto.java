02492da0a95f7b912af10c0471b011f6c1a4c4ef
dlxodnd007 <dlxodnd007@naver.com>
2024-08-14 18:19:17 +0900

    0814_1

---
+++ 07_Spring/spring-afternoon/src/main/java/org/example/dto/board/BoardDto.java
@@ -0,0 +1,22 @@
+package org.example.dto.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BoardDto {
+    private Long no;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regDate;
+    private Date updateDate;
+}
